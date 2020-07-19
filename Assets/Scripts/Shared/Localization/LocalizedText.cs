b61cc6ef88eaa506395d0fa54653209a68965cd9
Haetham <g3agle@gmail.com>
2020-07-19 17:43:47 +0300

    Setup localization, 2d IK and added boy and girl spritesheets with rigging

--- Assets/Scripts/Shared/Localization/LocalizedText.cs
+++ Assets/Scripts/Shared/Localization/LocalizedText.cs
@@ -2,37 +2,39 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
-using TMPro;
+//using TMPro;
 
 public class LocalizedText : MonoBehaviour
 {
 
-    public TMP_FontAsset[] font;
-    TextMeshProUGUI text;
+    //public TMP_FontAsset[] font;
+    //TextMeshProUGUI text;
+    Text text;
     string chosenLanguage;
     public string key;
 
-    private void Awake()
-    {
-       chosenLanguage = PlayerPrefs.GetString("locale");
-       text = GetComponent<TextMeshProUGUI>();
+    //private void Awake()
+    //{
+    //   chosenLanguage = PlayerPrefs.GetString("locale");
+    //   text = GetComponent<TextMeshProUGUI>();
 
-        if (chosenLanguage == "localizedText_en.json")
-        {
-            text.font = font[0];
-        } else if (chosenLanguage == "localizedText_ar.json")
-        {
-            text.font = font[1];
-        } else
-        {
-            text.font = font[0];
-        }
-    }
+    //    if (chosenLanguage == "localizedText_en.json")
+    //    {
+    //        text.font = font[0];
+    //    } else if (chosenLanguage == "localizedText_ar.json")
+    //    {
+    //        text.font = font[1];
+    //    } else
+    //    {
+    //        text.font = font[0];
+    //    }
+    //}
 
     // Start is called before the first frame update
     void Start()
-    { 
-        text.text = LocalizationManager.instance.GetLocalizedValue(key);
+    {
+        //text = GetComponent<Text>();
+        //text.text = LocalizationManager.instance.GetLocalizedValue(key);
     }
      
 }
